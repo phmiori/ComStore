b2c3a071aefe83f02f460a9909660cdece04ee6d
Scramjat <davioliveirapereira05@gmail.com>
2025-04-28 20:08:00 -0300

    carrossel e listas funcionais
    Adição de carrossel e de algumas listas que ao clicar entra na pagina daqueles respequitivos produtos

--- script.js
+++ script.js
@@ -47,4 +47,76 @@ dropdownContent.addEventListener('mouseleave', () => {
   dropdownContent.style.display = 'none'; // Esconde a lista quando o mouse sai da lista
 });
 
+/*-------------------------------------------------------------*/
+/*-------------------------------------------------------------*/
+
+const track = document.getElementById('carousel-track');
+const imagesContainer = document.getElementById('carousel-images');
+let currentIndexe = 0;
+let isPaused = false;
+
+// Calcula quantas imagens existem
+const images = imagesContainer.querySelectorAll('img');
+const imageWidth = images[0].clientWidth + 10; // Imagem + margin-right
+
+// Duplicar as imagens dinamicamente no JS para permitir loop contínuo
+images.forEach(img => {
+  const clone = img.cloneNode(true);
+  imagesContainer.appendChild(clone);
+});
+
+// Função para mover automaticamente
+function autoMove() {
+  if (isPaused) return;
+
+  currentIndex++;
+  track.style.transition = "transform 0.5s ease-in-out";
+  track.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+
+  // Quando chega no final das imagens clonadas
+  if (currentIndex >= images.length) {
+    setTimeout(() => {
+      track.style.transition = "none"; // tira a transição para voltar sem animação
+      currentIndex = 0;
+      track.style.transform = `translateX(0px)`;
+    }, 500); // espera a animação acabar
+  }
+}
+
+// Timer para o movimento automático
+let moveInterval = setInterval(autoMove, 2000); // a cada 2s move
+
+// Função para pausar no hover
+carousel.addEventListener('mouseenter', () => {
+  isPaused = true;
+});
+carousel.addEventListener('mouseleave', () => {
+  isPaused = false;
+});
+
+// Função para mover manualmente com os botões
+function moveSlideManual(direction) {
+  pauseAndResume(); // Pausa o automático ao clicar
+
+  currentIndex += direction;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  } else if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+
+  track.style.transition = "transform 0.5s ease-in-out";
+  track.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+}
+
+// Função que pausa e retoma depois de clicar
+function pauseAndResume() {
+  clearInterval(moveInterval);
+  moveInterval = setInterval(autoMove, 2000);
+}
+
+
+/*-------------------------------------------------------------*/
+/*-------------------------------------------------------------*/
+
 
